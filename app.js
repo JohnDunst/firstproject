ecd471363eb49552f793146cd5e003f93fd2b5b1
John <johnnydunst6@outlook.com>
2023-06-26 12:05:16 -0400

    first statement

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 // Need to set values for player and CPU
 
 
-
-
 //start function IE: player click on selection button
+const userScore = 0;
+const cpuScore=0;
 
 
 
 
 
 //logic of the game similar to my mini project code logic
+        if (userChoice===cpuChoice){
+            return "Tie";
+        }
+        else if (userChoice==rock);
+            if(cpuChoice==scissors)
+            console.log('you win');
+            else if (cpuChoice==paper);
+            console.log('cpu win');
+            
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+    
 // TIES
 
 //reload button
@@ -20,5 +41,3 @@
 //Display round count
 
 //Display score
-
-
