90a027a039b3dfeabc93c2c9e29243665d031f37
John <johnnydunst6@outlook.com>
2023-06-26 11:09:57 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Need to set values for player and CPU
+
+
+
+
+//start function IE: player click on selection button
+
+
+
+
+
+//logic of the game similar to my mini project code logic
+
+
+
+// TIES
+
+//reload button
+
+//Display round count
+
+//Display score
+
+
