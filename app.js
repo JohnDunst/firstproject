6d015cfd05e46cdc1ca4a83aae426be973cc40fd
John <johnnydunst6@outlook.com>
2023-06-27 14:03:37 -0400

    almost got jt

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
 // Need to set values for player and CPU
-
+let userScore = 0;
+let cpuScore=0;
+let cpuChoice=0;
 
 //start function IE: player click on selection button
-const game = () =>{
-const userScore = 0;
-const cpuScore=0;
+
+const rockBtn = document.querySelector ('rock');
+const paperBtn = document.querySelector ('paper');
+const sicssorsBtn = document.querySelector ('scissors');
+const choiceList = ("rock", "paper", "scissors");
+
+
+function cpuChoices (){
+    cpuChoice= choiceList [Math.floor(Math.random()*3)];
+    return cpuChoice;
+}
 
 
 
-
-const playgame =() => {
-        const rockBtn = document.querySelector ('rock');
-        const paperBtn = document.querySelector ('paper');
-        const sicssorsBtn = document.querySelector ('scissors');
     
-const choiceList = new Array("rock", "paper", "scissors")
-    
-const cpuChoice=choiceList[Math.ceil(Math.random()*3)]
 
 //logic of the game similar to my mini project code logic
     if (userChoice===cpuChoice){
             return "Tie";
 
-    }else if (userChoice==rock);{
-            if(cpuChoice==scissors){
+    }else if (userChoice===rock);{
+            if(cpuChoice===scissors){
             console.log('you win');
-            }else if (cpuChoice==paper);
+            userScore ++;
+            }else if (cpuChoice===paper);
             console.log('cpu win');
+            cpuScore++;
         }
-    } 
-    if (userChoice==paper);{
-            if(cpuChoice==rock)
+     
+    if (userChoice===paper);{
+            if(cpuChoice===rock){
             console.log('you win');
-            else if (cpuChoice==scissors);
+            userScore ++;
+            }else if (cpuChoice==scissors);
             console.log('cpu win');
+            cpuScore ++;
+        
 
-    if (userChoice==scissors);
-            if(cpuChoice==paper)
+    if (userChoice===scissors);
+            if(cpuChoice===paper)
             console.log('you win');
             else if (cpuChoice==rock);
             console.log('cpu win');
@@ -47,7 +54,7 @@ const cpuChoice=choiceList[Math.ceil(Math.random()*3)]
     
     
     
-    }
+    
 
 
 
