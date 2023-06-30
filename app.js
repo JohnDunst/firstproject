94a60e864d5ea7b429629ae51d6d00ae81a31d00
John <johnnydunst6@outlook.com>
2023-06-30 14:41:27 -0400

    counter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Need to set values for player and CPU
-let userScore = 0;
-let cpuScore = 0;
+let uScore = 0;
+let cScore = 0;
 //let cpuChoice="";
 //let userChoice="";
 //start function IE: player click on selection button
@@ -11,6 +11,14 @@ const sicssorsBtn = document.querySelector(".scissors");
 const choiceList = ["rock", "paper", "scissors"];
 userChoice = (rockBtn, paperBtn, sicssorsBtn);
 
+
+const updateScores = () =>{
+    const userScore = document.querySelector(".userScore p");
+    const cpuScore = document.querySelector(".cpuScore p");
+    userScore.textContent = uScore;
+    cpuScore.textContent = cScore;
+
+};
 /*et cpuChoices =
 random=(Math.floor(Math.random()*3));
 cpuChoice= choiceList[random];*/
@@ -31,13 +39,13 @@ paperBtn.addEventListener("click", (evt) => {
   console.log("paper");
   let choice = cpuChoices();
   console.log(choice);
-  var result = winner("paper",choice);
+  var result = winner("paper", choice);
 });
 sicssorsBtn.addEventListener("click", (evt) => {
   console.log("sicssors");
-let choice = cpuChoices();
-console.log(choice);
-var result = winner("scissors",choice);
+  let choice = cpuChoices();
+  console.log(choice);
+  var result = winner("scissors", choice);
 });
 
 //logic of the game similar to my mini project code logic
@@ -48,24 +56,41 @@ const winner = (userChoice, cpuChoice) => {
     if (userChoice === "rock") {
       if (cpuChoice === "scissors") {
         console.log("you win");
-        userScore++;
-      } else{ //cpuChoice === "paper";
-      console.log("cpu win");
-      cpuScore++;}
+        uScore++;
+        updateScores();
+
+      } else {
+        //cpuChoice === "paper";
+        console.log("cpu win");
+        cScore++;
+        updateScores();
+
+      }
     } else if (userChoice === "paper") {
       if (cpuChoice === "rock") {
         console.log("you win");
-        userScore++;
-      } else {//cpuChoice == "scissors";
-      console.log("cpu win");
-      cpuScore++;}
+        uScore++;
+        updateScores();
+
+      } else {
+        //cpuChoice == "scissors";
+        console.log("cpu win");
+        cScore++;
+        updateScores();
+
+      }
     } else if (userChoice === "scissors") {
       if (cpuChoice === "paper") {
         console.log("you win");
-        userScore++;
-      } else{ //cpuChoice == "rock";
-      console.log("cpu win");
-      cpuScore++;}
+        uScore++;
+        updateScores();
+
+      } else {
+        //cpuChoice == "rock";
+        console.log("cpu win");
+        cScore++;
+        updateScores();
+      }
     }
   }
 };
@@ -75,4 +100,4 @@ const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", (evt) => {
   location.reload();
 });
-//roundsfunction game() {
+//roundsfunction game() 
