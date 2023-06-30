e26198cd45ee520609e08e070a11ae18e41489d6
John <johnnydunst6@outlook.com>
2023-06-30 14:23:41 -0400

    logic sounds

--- app.js
+++ app.js
@@ -1,83 +1,78 @@
 // Need to set values for player and CPU
 let userScore = 0;
-let cpuScore=0;
-let cpuChoice="";
-let userChoice="";
+let cpuScore = 0;
+//let cpuChoice="";
+//let userChoice="";
 //start function IE: player click on selection button
 
-const rockBtn = document.querySelector ('.rock');
-const paperBtn = document.querySelector ('.paper');
-const sicssorsBtn = document.querySelector ('.scissors');
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const sicssorsBtn = document.querySelector(".scissors");
 const choiceList = ["rock", "paper", "scissors"];
-rockBtn.addEventListener("click",(evt)=> {
-    console.log("rockBtn");
-    cpuChoices()
-    console.log(cpuChoice);
-})
-paperBtn.addEventListener("click",(evt)=> {
-    console.log("paperBtn");
+userChoice = (rockBtn, paperBtn, sicssorsBtn);
 
-})
-sicssorsBtn.addEventListener("click",(evt)=> {
-    console.log("sicssorsBtn");
+/*et cpuChoices =
+random=(Math.floor(Math.random()*3));
+cpuChoice= choiceList[random];*/
 
-})
-
-
-function cpuChoices (){
-let random=(Math.floor(Math.random()*3));
-    cpuChoice= choiceList[random];
-    return cpuChoice;
+function cpuChoices() {
+  random = Math.floor(Math.random() * 3);
+  return choiceList[random];
 }
 
-
-   
+rockBtn.addEventListener("click", (evt) => {
+  console.log("rock");
+  let choice = cpuChoices();
+  console.log(choice);
+  var result = winner("rock", choice);
+  //console.log(winner);
+});
+paperBtn.addEventListener("click", (evt) => {
+  console.log("paper");
+  let choice = cpuChoices();
+  console.log(choice);
+  var result = winner("paper",choice);
+});
+sicssorsBtn.addEventListener("click", (evt) => {
+  console.log("sicssorsBtn");
+let choice = cpuChoices();
+console.log(choice);
+var result = winner("scissors",choice);
+});
 
 //logic of the game similar to my mini project code logic
-const winner= (userChoice,cpuChoice)=>{   
-if (userChoice===cpuChoice){
-            console.log("tie");
-    }else if (userChoice==='rock');{
-            if(cpuChoice==='scissors'){
-            console.log("you win");
-            userScore ++;
-            }else if (cpuChoice==='paper');
-            console.log("cpu win");
-            cpuScore++;
-        }
-     
-    if (userChoice==='paper');{
-            if(cpuChoice==='rock'){
-            console.log("you win");
-            userScore ++;
-            }else if (cpuChoice=='scissors');
-            console.log("cpu win");
-            cpuScore ++;
+const winner = (userChoice, cpuChoice) => {
+  if (userChoice === cpuChoice) {
+    console.log("tie");
+  } else {
+    if (userChoice === "rock") {
+      if (cpuChoice === "scissors") {
+        console.log("you win");
+        userScore++;
+      } else{ //cpuChoice === "paper";
+      console.log("cpu win");
+      cpuScore++;}
+    } else if (userChoice === "paper") {
+      if (cpuChoice === "rock") {
+        console.log("you win");
+        userScore++;
+      } else {//cpuChoice == "scissors";
+      console.log("cpu win");
+      cpuScore++;}
+    } else if (userChoice === "scissors") {
+      if (cpuChoice === "paper") {
+        console.log("you win");
+        userScore++;
+      } else{ //cpuChoice == "rock";
+      console.log("cpu win");
+      cpuScore++;}
     }
-
-    if (userChoice==='scissors');{
-            if(cpuChoice==='paper'){
-            console.log("you win");
-            userScore ++;
-            }else if (cpuChoice=='rock');
-            console.log("cpu win");
-            cpuScore ++;
-    }
-}
-    
-    
-    
-    
-    
-    
-
-
-
-
-
+  }
+};
 
 //reload button
-
-//Display round count
-
-//Display score
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", (evt) => {
+  location.reload();
+});
+//roundsfunction game() {
