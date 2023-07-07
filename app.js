e35ba5bc8b0cf3a461e68e2211ce3b4743602c89
John <johnnydunst6@outlook.com>
2023-07-07 15:49:17 -0400

    results displayed

--- app.js
+++ app.js
@@ -10,15 +10,9 @@ const paperBtn = document.querySelector(".paper");
 const sicssorsBtn = document.querySelector(".scissors");
 const choiceList = ["rock", "paper", "scissors"];
 userChoice = (rockBtn, paperBtn, sicssorsBtn);
+const resultText = document.querySelector(".resultText");
 
 
-const updateScores = () =>{
-    const userScore = document.querySelector(".userScore p");
-    const cpuScore = document.querySelector(".cpuScore p");
-    userScore.textContent = uScore;
-    cpuScore.textContent = cScore;
-
-};
 /*et cpuChoices =
 random=(Math.floor(Math.random()*3));
 cpuChoice= choiceList[random];*/
@@ -33,6 +27,8 @@ rockBtn.addEventListener("click", (evt) => {
   let choice = cpuChoices();
   console.log(choice);
   var result = winner("rock", choice);
+
+
   //console.log(winner);
 });
 paperBtn.addEventListener("click", (evt) => {
@@ -52,52 +48,86 @@ sicssorsBtn.addEventListener("click", (evt) => {
 const winner = (userChoice, cpuChoice) => {
   if (userChoice === cpuChoice) {
     console.log("tie");
+    {return resultText.textContent= "TIE";
+
+}
+    
   } else {
     if (userChoice === "rock") {
       if (cpuChoice === "scissors") {
         console.log("you win");
         uScore++;
         updateScores();
+        {return resultText.textContent= "UserWin!";
 
+      }
       } else {
         //cpuChoice === "paper";
         console.log("cpu win");
         cScore++;
         updateScores();
+        {return resultText.textContent= "CPUWIN";
 
+      }
       }
     } else if (userChoice === "paper") {
       if (cpuChoice === "rock") {
         console.log("you win");
         uScore++;
         updateScores();
+        {return resultText.textContent= "UserWin!";
+
+    }
 
       } else {
         //cpuChoice == "scissors";
         console.log("cpu win");
         cScore++;
         updateScores();
+        {return resultText.textContent= "CPUWIN";
 
+      }
       }
     } else if (userChoice === "scissors") {
       if (cpuChoice === "paper") {
         console.log("you win");
         uScore++;
         updateScores();
+        {return resultText.textContent= "UserWin!";
 
+      }
       } else {
         //cpuChoice == "rock";
         console.log("cpu win");
         cScore++;
         updateScores();
+        {return resultText.textContent= "CPUWIN";
+
+    }
       }
     }
   }
 };
+const updateScores = () =>{
+    const userScore = document.querySelector(".userScore p");
+    const cpuScore = document.querySelector(".cpuScore p");
+    userScore.textContent = uScore;
+    cpuScore.textContent = cScore;
 
+};
 //reload button
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", (evt) => {
   location.reload();
 });
 //roundsfunction game() 
+const userWin = ()=>{
+if (uScore = 5){
+    {return resultText.textContent= "Match over";
+
+}   window.alert("Hello! I am an alert box!!");
+   console.log("Winner einner");
+}
+}
+//resultText.textContent= checkwinner();
+
